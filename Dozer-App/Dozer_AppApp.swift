f1cf9f9ec025ef6dd40b85ae979552cebc129045
Jasper Vandenbroucke <jasper.vandenbroucke@student.hogent.be>
2024-12-21 07:05:10 -0800

    Initial Commit

---
+++ Dozer-App/Dozer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dozer_AppApp.swift
+//  Dozer-App
+//
+//  Created by Jasper Vandenbroucke on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dozer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
