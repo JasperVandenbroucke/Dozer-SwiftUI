a7ac9eff4b6b05fc572158cb0497b2c443481f4f
Jasper Vandenbroucke <jasper.vandenbroucke@student.hogent.be>
2025-01-04 17:52:05 -0800

    Layout of the addMachineView

--- Dozer-App/Views/AddMachineView.swift
+++ Dozer-App/Views/AddMachineView.swift
@@ -1,8 +1,49 @@
 import SwiftUI
 
 struct AddMachineView: View {
+    
+    @State var machineName: String = ""
+    @State var machineType: String = ""
+    @State var machinePrice: Double = 0.0
+    
+    @State var optionName: String = ""
+    @State var optionPrice: Double = 0.0
+    
+    @State private var options: [Option] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Machine Info")) {
+                TextField("Machine Name", text: $machineName)
+                TextField("Machine Type", text: $machineType)
+                //TextField("Price", text: $machinePrice)
+            }
+            Section(header: Text("Machine Options")) {
+                TextField("Option Name", text: $optionName)
+                //TextField("Option Price", text: $optionPrice)
+                Button("Add Option") {
+                    addOption()
+                }
+            }
+            Section {
+                Text("Amount of options: \(options.count)")
+            }
+            Button("Add Machine") {
+                addMachine()
+            }
+        }
+    }
+    
+    func addOption() {
+        if !optionName.isEmpty && !optionPrice.isNaN {
+            options.append(Option(optionName: optionName, price: optionPrice))
+            optionName = ""
+            optionPrice = 0.0
+        }
+    }
+    
+    func addMachine() {
+        // TODO: IMPLEMENT AN ADD MACHINE (PASSING VALUES NO MACHINE)
     }
 }
 
